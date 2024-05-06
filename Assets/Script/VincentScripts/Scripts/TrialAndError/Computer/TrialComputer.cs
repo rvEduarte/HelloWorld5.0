7f6609931a0c434e92418a51b0223beb62f0c1d9
DESKTOP-VL6SNKH\WIN10 <rveduarte@gmail.com>
2024-05-06 07:39:13 -0400

    05/06/2024 Updates: ADDED FUNCTIONALITY TO THE GUN AND COMPUTER

---
+++ Assets/Script/VincentScripts/Scripts/TrialAndError/Computer/TrialComputer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TrialComputer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletHelloWrite;
+    public TMP_Text canvasText;
+
+    string helloWriteLine = "HelloWorld";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("ENTER BULLET TITE");
+        //if the game object we collided with is not equal to the current game object (so anything other than itself)
+        if (collision.gameObject.tag == "WLHELLO")
+        {
+            //canvasText.text = "\nPEPE ";
+            canvasText.text = helloWriteLine;
+        }
+    }
+}
