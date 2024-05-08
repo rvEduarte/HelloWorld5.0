8e7d20999e3e9d96ec5411a931e1b8ef7efc2100
DESKTOP-VL6SNKH\WIN10 <rveduarte@gmail.com>
2024-05-08 08:35:58 -0400

    05/08/2024 Updates: FINALLY DONE THE FUNCTIONALITY OF HINTBUTTON.

--- Assets/Script/VincentScripts/Scripts/TrialAndError/HintButton.cs
+++ Assets/Script/VincentScripts/Scripts/TrialAndError/HintButton.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HintButton : MonoBehaviour
 {
     public Button ButtonHint;
     public GameObject DisablePanel;
+
+    public TMP_Text canvasText1;
+    public TMP_Text canvasText2;
+    public TMP_Text canvasText3;
+    public TMP_Text canvasText4;
 
     public bool clicked = true;
 
@@ -24,11 +30,24 @@ public class HintButton : MonoBehaviour
         if (clicked == true)
         {
             DisablePanel.SetActive(false);
+            //string color = "#18eded";
+            canvasText1.text = "" + "Write" + "(\"HELLOWORLD\")";
+            canvasText2.text = "" + "WriteLine" + "(\"HELLOWORLD\")";
+            canvasText3.text = "" + "Write" + "( 4 + 3 )";
+            canvasText4.text = "" + "WriteLine" + "( 4 + 3 )";
+
         }
 
         else if (clicked == false)
         {
             DisablePanel.SetActive(true);
+            string color = "#FF0000";
+            string color1 = "#0000FF";
+            canvasText1.text = "" + "<color=" + color + ">Write</color>" + "(\"HELLOWORLD\")";
+            canvasText2.text = "" + "<color=" + color1 + ">WriteLine</color>" + "(\"HELLOWORLD\")";
+            canvasText3.text = "" + "<color=" + color + ">Write</color>" + "( 4 + 3 )";
+            canvasText4.text = "" + "<color=" + color1 + ">WriteLine</color>" + "( 4 + 3 )";
+
         }
     }
 
