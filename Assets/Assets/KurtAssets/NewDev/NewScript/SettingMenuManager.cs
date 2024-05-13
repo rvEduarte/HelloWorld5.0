207a46efc03a898af5d574ce643319c1ade36a87
Users/user <kurtaquino13@gmail.com>
2024-05-13 17:23:28 +0800

    05132024 Updates: Editing Main Scene/Scene 1/ Settings Master, Music, SFX Volume Slider (Incomplete)

--- Assets/Assets/KurtAssets/NewDev/NewScript/SettingMenuManager.cs
+++ Assets/Assets/KurtAssets/NewDev/NewScript/SettingMenuManager.cs
@@ -2,16 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 using TMPro;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     public TMP_Dropdown graphicsDropdown;
+    public Slider masterVol, musicVol, sfxVol;
+    public AudioMixer settingAudioMixer;
 
 
     public void ChangeGraphicsQuality()
     {
         QualitySettings.SetQualityLevel(graphicsDropdown.value);
+    }
+
+    public void ChangeMasterVolume()
+    {
+        settingAudioMixer.SetFloat("MasterV", masterVol.value);
+    }
+
+    public void ChangeMusicVolume()
+    {
+        settingAudioMixer.SetFloat("MusicV", musicVol.value);
+    }
+
+    public void ChangeSFXVolume()
+    {
+        settingAudioMixer.SetFloat("MyExposedParam 2", sfxVol.value);
     }
 
 
