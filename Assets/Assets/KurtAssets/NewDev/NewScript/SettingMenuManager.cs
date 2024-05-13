cfa48d14cf548d3277182a5ab02404da33f14d32
Users/user <kurtaquino13@gmail.com>
2024-05-13 12:33:08 +0800

    051320204 Updates: Edited Main Scene Folder/Scene1/Settings/DropdownGraphics (Successfully)

---
+++ Assets/Assets/KurtAssets/NewDev/NewScript/SettingMenuManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public TMP_Dropdown graphicsDropdown;
+
+
+    public void ChangeGraphicsQuality()
+    {
+        QualitySettings.SetQualityLevel(graphicsDropdown.value);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
