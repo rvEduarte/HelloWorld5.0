936afbcf01b83fd4b091a8f8b39e630bd56e4c71
Users/user <kurtaquino13@gmail.com>
2024-05-20 20:47:30 +0800

    05202024 Updates: VincentScript/Lvl1/ Edit timer

---
+++ Assets/Assets/KurtAssets/NewDev/NewScript/RunningTimer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+
+public class RunningTimer : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI timerTxt;
+ 
+    float elapsedTime;
+
+  
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        timerTxt.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+}
