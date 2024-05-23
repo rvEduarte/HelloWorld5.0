4d5abb65037688dc507244533518b151fcf0babe
DESKTOP-MVDTUTR\Rv <rveduarte@gmail.com>
2024-05-23 16:55:55 -0700

    05/24/2024 Updates: ADDED SCRIPT: QUIZMANAGE FOR LEVEL1_PH3

---
+++ Assets/Script/VincentScripts/Scripts/QuizScript/QuizManage.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
